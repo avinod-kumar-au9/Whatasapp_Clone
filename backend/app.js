45e571126bc60b1bbe94f9c94e8f9946c14a8279
A vinod kumar <asundi.vinodkumar888@gmail.com>
2021-02-14 10:44:54 +0530

    sdf

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const mongo = require("mongodb");
 const mongoClient = mongo.MongoClient;
 const bodyParser = require("body-parser");
